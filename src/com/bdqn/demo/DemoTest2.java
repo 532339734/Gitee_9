942a4861a54ffbc5db9893bb6d6e758b2e85c7b8
曾攀辉 <532339734@qq.com>
2021-12-08 10:12:06 +0800

    hot-fix3

--- src/com/bdqn/demo/DemoTest2.java
+++ src/com/bdqn/demo/DemoTest2.java
@@ -9,10 +9,7 @@ package com.bdqn.demo;
 
 public class DemoTest2 {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println(3333333);
+        System.out.println("hot-fix test");
+
     }
 }
