d8691d6868c85250e9dd61dc2ea8e6386f032309
曾攀辉 <532339734@qq.com>
2021-12-08 10:14:04 +0800

    master

--- src/com/bdqn/demo/DemoTest2.java
+++ src/com/bdqn/demo/DemoTest2.java
@@ -9,12 +9,8 @@ package com.bdqn.demo;
 
 public class DemoTest2 {
     public static void main(String[] args) {
-
-        System.out.println("hello git1");
-        System.out.println("hello git1");
-        System.out.println("hello git1");
-        System.out.println("hello git1");
-        System.out.println("hello git1");
+        System.out.println(1234);
+        System.out.println("master test");
 
 
     }
