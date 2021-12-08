f65400addeb35b42d3d97856b59390f65bea64b9
曾攀辉 <532339734@qq.com>
2021-12-08 10:12:56 +0800

    hot-fix4

--- src/com/bdqn/demo/DemoTest2.java
+++ src/com/bdqn/demo/DemoTest2.java
@@ -9,6 +9,7 @@ package com.bdqn.demo;
 
 public class DemoTest2 {
     public static void main(String[] args) {
+        System.out.println("1234");
         System.out.println("hot-fix test");
 
     }
